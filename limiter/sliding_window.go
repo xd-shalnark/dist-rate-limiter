37a5cbf32bca416121f3a0e43af9e4b11c873765
xd-shalnark <243507956+xd-shalnark@users.noreply.github.com.>
2026-07-21 20:57:36 +0200

    commit

--- limiter/sliding_window.go
+++ limiter/sliding_window.go
@@ -1,14 +1,14 @@
 package limiter
 
-import (   // Importing necessary packages
+import ( // Importing necessary packages
 	"sync"
-	"time"
+	"time" //commit
 )
 
 type SlidingWindowLimiter struct {
 	mu          sync.Mutex
 	prevCount   int
-	currCount   int 		//Limiter structure
+	currCount   int //Limiter structure
 	limit       int
 	windowSize  time.Duration
 	windowStart time.Time
@@ -16,29 +16,29 @@ type SlidingWindowLimiter struct {
 
 func (l *SlidingWindowLimiter) Allow() bool {
 	l.mu.Lock()
-	defer l.mu.Unlock() 		// Locking to ensure thread safety
+	defer l.mu.Unlock() // Locking to ensure thread safety
 
 	now := time.Now()
 	elapsed := now.Sub(l.windowStart)
 
 	if elapsed >= l.windowSize {
-    windowsPassed := int(elapsed / l.windowSize)
-    if windowsPassed == 1 {
-        l.prevCount = l.currCount           
-    } else {
-        l.prevCount = 0 
-    }
-    l.currCount = 0
-    l.windowStart = l.windowStart.Add(time.Duration(windowsPassed) * l.windowSize)
-    elapsed = now.Sub(l.windowStart)
-}
+		windowsPassed := int(elapsed / l.windowSize)
+		if windowsPassed == 1 {
+			l.prevCount = l.currCount
+		} else {
+			l.prevCount = 0
+		}
+		l.currCount = 0
+		l.windowStart = l.windowStart.Add(time.Duration(windowsPassed) * l.windowSize)
+		elapsed = now.Sub(l.windowStart)
+	}
 	progress := float64(elapsed) / float64(l.windowSize)
-	prevWeight := 1.0 - progress   //Mathematical part
+	prevWeight := 1.0 - progress //Mathematical part
 	expectedCount := float64(l.prevCount)*prevWeight + float64(l.currCount)
 
 	if expectedCount >= float64(l.limit) {
 		return false
-	}                 					// allow/deny
+	} // allow/deny
 	l.currCount++
 	return true
 }
